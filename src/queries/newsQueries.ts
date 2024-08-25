14487fffdffedb8c8fa8b3b6c58c374dca4ea200
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-08-25 16:50:33 -0700

    Update api error messages

--- src/queries/newsQueries.ts
+++ src/queries/newsQueries.ts
@@ -3,21 +3,16 @@ import axios from "axios";
 export const fetchSentiments = async () => {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/article/sentiments`);
-    const res_data = response.data
-    if (response.status === 200 && res_data) {
-      const sentiment_options = res_data.sentiment
-      return {
-        Positive: new Set(sentiment_options.Positive),
-        Negative: new Set(sentiment_options.Negative),
-        Neutral: new Set(sentiment_options.Neutral),
-      };
-    } else {
-      throw new Error("Failed to fetch sentiments: Invalid response data");
+    if (response.status != 200) {
+      throw new Error(`Sentiment list request returned with status ${response.status}`);
     }
+    const sentiment_options = response.data.sentiment;
+    return {
+      Positive: new Set(sentiment_options.Positive),
+      Negative: new Set(sentiment_options.Negative),
+      Neutral: new Set(sentiment_options.Neutral),
+    };
   } catch (error) {
     console.error("Error fetching sentiment options:", error);
-    throw error;
   }
 };
-
-export default fetchSentiments;
--- src/queries/stockQueries.ts
+++ src/queries/stockQueries.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 export const fetchTickerList = async () => {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/stock/tickers`);
-    const res_data = response.data
-    if (response.status === 200 && res_data) {
-      return res_data.tickers
+    if (response.status != 200) {
+      throw new Error(`Ticker list request returned with status ${response.status}`);
     }
+    return response.data.tickers;
   } catch (error) {
     console.error("Error fetching tickers:", error);
   }
@@ -15,9 +15,11 @@ export const fetchTickerList = async () => {
 export const fetchQuoteInfo = async (ticker: string) => {
   try {
     const response = await axios.get(`/api/stock/quote?ticker=${ticker}`);
+    if (response.status != 200) {
+      throw new Error(`Finnhub quote request returned with status ${response.status}`);
+    }
     return response.data.quoteInfo;
   } catch (error) {
     console.error(`Error fetching quote info for ${ticker}:`, error);
-    throw error;
   }
 };
