b69f1db60851a8ea6fef7fa4aec444bb5bcaafd1
Marvin Deng <mdengomg2@gmail.com>
2026-07-03 20:36:20 -0700

    Fix type error

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -25,15 +25,13 @@ const Navbar = () => {
         {NAV_LINKS.map(({ label, href }) => (
           <Box
             key={href}
-            as={Link}
-            href={href}
             fontWeight={pathname === href ? "bold" : "normal"}
             borderBottom={pathname === href ? "2px solid" : "2px solid transparent"}
             borderColor={pathname === href ? "blue.500" : "transparent"}
             pb={1}
             _hover={{ color: "blue.500" }}
           >
-            {label}
+            <Link href={href}>{label}</Link>
           </Box>
         ))}
       </Flex>
