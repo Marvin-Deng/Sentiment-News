82cb0af04cbbccae7e010d0e45b2ad8803feb27d
Marvin Deng <mdengomg2@gmail.com>
2026-07-07 00:15:08 -0700

    Debounce search results

--- client/src/components/Navbar/SearchBar.tsx
+++ client/src/components/Navbar/SearchBar.tsx
@@ -1,15 +1,31 @@
 "use client";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { Input, IconButton } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 import { SearchContext } from "../../providers/SearchProvider";
 
+const DEBOUNCE_MS = 300;
+
 const SearchBar = () => {
   const [searchString, setSearchString] = useState<string>("");
   const ctx = useContext(SearchContext);
+  const debounceRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  useEffect(() => {
+    if (debounceRef.current) clearTimeout(debounceRef.current);
+    debounceRef.current = setTimeout(() => {
+      ctx?.updateSearchQuery(searchString);
+    }, DEBOUNCE_MS);
+
+    return () => {
+      if (debounceRef.current) clearTimeout(debounceRef.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchString]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (debounceRef.current) clearTimeout(debounceRef.current);
     ctx?.updateSearchQuery(searchString);
   };
 
